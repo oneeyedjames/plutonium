ac513b88f86266a563830fbea425170632cc091d
radioactivepig <radioactivepig@f552c862-8d41-41d5-8bd0-1969eaec2ec4>
2012-07-06 20:40:22 +0000

    URL parsing refitted for Nginx

--- application/request.php
+++ application/request.php
@@ -2,44 +2,47 @@
 
 $uri['host'] = $_SERVER['SERVER_NAME'];
 $uri['root'] = dirname($_SERVER['SCRIPT_NAME']);
-$uri['self'] = $_SERVER['PHP_SELF'];
 $uri['file'] = $_SERVER['SCRIPT_NAME'];
 $uri += parse_url($_SERVER['REQUEST_URI']);
 
+$base = explode('.', $config['system']['hostname']);
 $host = explode('.', $_SERVER['SERVER_NAME']);
+$path = explode(FS, trim($uri['path'], FS));
 
 if ($host[0] == 'www') array_shift($host);
 
-if (count($host) >= 3) $_REQUEST['site'] = $host[count($host) - 3];
+$host = array_reverse($host);
 
-if ($uri['self'] != $uri['path'] && $uri['self'] == $uri['file']) {
-	$path = trim(str_replace(dirname($uri['file']), '', $uri['path']), '/');
+foreach (array_reverse($base) as $base_slug) {
+	if ($host[0] == $base_slug) array_shift($host);
+	else die('Improperly formed hostname');
 }
 
-if ($uri['self'] == $uri['path'] && $uri['self'] != $uri['file']) {
-	$path = trim(str_replace($uri['file'], '', $uri['path']), '/');
+if (in_array($path[0], array('', 'index.php'))) array_shift($path);
+
+if (!empty($host)) {
+	if (isset($host[0])) $_REQUEST['site']     = $host[0];
+	if (isset($host[1])) $_REQUEST['language'] = $host[1];
 }
 
-if (isset($path) && !empty($path)) {
-	$args = explode('/', $path);
+if (!empty($path)) {
+	if (isset($path[0])) $_REQUEST['module']   = $path[0];
+	if (isset($path[1])) $_REQUEST['resource'] = $path[1];
+	if (isset($path[2])) $_REQUEST['action']   = $path[2];
 	
-	if (isset($args[0])) $_REQUEST['module']   = $args[0];
-	if (isset($args[1])) $_REQUEST['resource'] = $args[1];
-	if (isset($args[2])) $_REQUEST['action']   = $args[2];
-	
-	if (isset($args[3])) {
-		if (($pos = strrpos($args[3], '.')) !== FALSE) {
-			$_REQUEST['slug']   = substr($args[3], 0, $pos);
-			$_REQUEST['format'] = substr($args[3], $pos + 1);
+	if (isset($path[3])) {
+		if (($pos = strrpos($path[3], '.')) !== FALSE) {
+			$_REQUEST['slug']   = substr($path[3], 0, $pos);
+			$_REQUEST['format'] = substr($path[3], $pos + 1);
 		} else {
-			$_REQUEST['slug']   = $args[3];
+			$_REQUEST['slug']   = $path[3];
 			$_REQUEST['format'] = 'html';
 		}
+		
+		unset($pos);
 	}
-	
-	unset($args, $pos);
 }
 
-unset($uri, $host, $path);
+unset($uri, $base, $host, $path);
 
 ?>
