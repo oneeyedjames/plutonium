5a983815b760ce7856c9cbb213c0a11e58bfa65c
radioactivepig <radioactivepig@f552c862-8d41-41d5-8bd0-1969eaec2ec4>
2013-02-25 16:40:16 +0000

    consolidated bootstrapping

--- plutonium/trunk/index.php
+++ plutonium/trunk/index.php
@@ -7,8 +7,37 @@
  * @license		http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 
-require_once 'framework.php';
-require_once 'functions.php';
+// Initialize Platform
+define('PLUTONIUM_VERSION', '0.9.0');
+
+// Initialize Basic Constants
+define('DS', DIRECTORY_SEPARATOR);
+define('PS', PATH_SEPARATOR);
+define('LS', PHP_EOL);
+define('BS', '\\');
+define('FS', '/');
+
+// Initialize Environment Constants
+defined('P_ROOT_PATH') or define('P_ROOT_PATH', realpath($_SERVER['DOCUMENT_ROOT']));
+defined('P_BASE_PATH') or define('P_BASE_PATH', realpath(dirname($_SERVER['SCRIPT_FILENAME'])));
+
+$scheme = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+$host   = $_SERVER['SERVER_NAME'];
+$path   = str_replace(P_ROOT_PATH, '', P_BASE_PATH);
+$path   = str_replace(BS, FS, $path);
+$path   = trim($path, FS);
+
+defined('P_ROOT_URL') or define('P_ROOT_URL', $scheme . '://' . $host);
+defined('P_BASE_URL') or define('P_BASE_URL', P_ROOT_URL . FS . $path);
+defined('P_PATH_URL') or define('P_PATH_URL', $path);
+
+unset($scheme, $host, $path);
+
+// Initialize Library
+require_once P_BASE_PATH . '/libraries/Plutonium/Functions/Array.php';
+require_once P_BASE_PATH . '/libraries/Plutonium/Loader.php';
+
+Plutonium_Loader::autoload(P_BASE_PATH . '/libraries');
 
 // Initialize Components
 require_once 'application/config.php';
--- plutonium/trunk/libraries/Plutonium/Functions/Array.php
+++ plutonium/trunk/libraries/Plutonium/Functions/Array.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @version		0.9.0
- * @package		Plutonium
- * @author		J Andrew Scott
- * @copyright	Copyright (C) 2010 J Andrew Scott. All rights reserved.
- * @license		http://opensource.org/licenses/gpl-license.php GNU Public License
- */
 
 function is_assoc($var) {
 	if (!is_array($var) || empty($var)) return false;
