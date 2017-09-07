dcabf8b613b9a5c1a5558663184864252c0f4f98
J Andrew Scott <jascott@programmer.net>
2017-09-07 09:30:51 -0400

    Added initial setup screen to module

--- modules/setup/controllers/steps.php
+++ modules/setup/controllers/steps.php
@@ -1,5 +1,8 @@
 <?php
 
 class StepsController extends Plutonium_Module_Controller {
-
+    public function defaultAction() {
+        $request = $this->module->request;
+        var_dump($request);exit;
+    }
 }
---
+++ modules/setup/views/steps/layouts/default.html.php
@@ -0,0 +1,22 @@
+<h1>Setup: Database</h1>
+
+<form action="/" method="post">
+	<input type="hidden" name="module" value="setup" />
+	<input type="hidden" name="resource" value="steps" />
+	<input type="hidden" name="action" value="default" />
+	<fieldset>
+		<legend>Database Credentials</legend>
+		<div>
+			<label for="data_database">Database:</label><br>
+			<input id="data_database" type="text" name="data[database]" />
+		<div>
+			<label for="data_username">Username:</label><br />
+			<input id="data_username" type="text" name="data[username]" />
+		</div>
+		<div>
+			<label for="data_password">Password:</label><br />
+			<input id="data_password" type="password" name="data[password]" />
+		</div>
+		<div><input type="submit" value="Create" /></div>
+	</fieldset>
+</form>
