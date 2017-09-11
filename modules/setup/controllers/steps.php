70f1a5a8b833361b8b69bd8fe7236e6bd9995f20
J Andrew Scott <jascott@programmer.net>
2017-09-11 16:27:48 -0400

    Creating default host during setup

--- modules/setup/controllers/steps.php
+++ modules/setup/controllers/steps.php
@@ -4,15 +4,7 @@ class StepsController extends Plutonium_Module_Controller {
     public function databaseAction() {
         $data = $this->request->get('data');
 
-        $config = new Plutonium_Object(array(
-            'hostname' => $data['hostname'],
-            'username' => $data['username'],
-            'password' => $data['password'],
-            'dbname'   => $data['dbname'],
-            'driver'   => $data['driver']
-        ));
-
-        //header('Content-type: text/plain');
+        $config = new Plutonium_Object($data);
 
         Plutonium_Database_Adapter::getInstance($config);
 
@@ -28,6 +20,47 @@ class StepsController extends Plutonium_Module_Controller {
         Plutonium_Database_Table::getInstance('resources')->create();
         Plutonium_Database_Table::getInstance('widgets')->create();
 
-        die('It\'s Over');
+        $model = $this->getModel('hosts');
+
+        $hosts = $model->find(array(
+            'slug' => 'main'
+        ));
+
+        if (empty($hosts)) {
+            $host_data = array(
+                'name'         => 'Main Host',
+                'slug'         => 'main',
+                'descrip'      => 'The main host for this Plutonium instance',
+                'default'      => true,
+                'meta_keyword' => 'Plutonium',
+                'meta_descrip' => 'A Plutonium-Powered website'
+            );
+
+            $host = $model->save($host_data);
+        }
+
+        // TODO 'install' core components
+        // Module: system
+        // Module: site
+        // Module: blog (maybe)
+        // Theme: charcoal
+        // Widget: login
+        // Widget: menu (maybe)
+
+        // $file = PU_PATH_BASE . DS . 'config.php';
+        // $data = $this->buildConfig($data);
+
+        // file_put_contents($file, $data);
+    }
+
+    protected function buildConfig($config) {
+        $output = '<?php' . PHP_EOL . PHP_EOL;
+
+        foreach ($config as $key => $value)
+            $output .= '$config[\'database\'][\'' . $key . '\'] = \'' . $value . '\';' . PHP_EOL;
+
+        $output .= PHP_EOL;
+
+        return $output;
     }
 }
