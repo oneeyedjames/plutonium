c726c0ae76b688b0bdb1f274469528632f4d4bb9
J Andrew Scott <jascott@programmer.net>
2020-07-23 11:22:47 -0400

    Workspace for theme

---
+++ src/themes/barista/layouts/default.html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<pu:head>
+		<link rel="stylesheet" href="themes/barista/assets/barista.min.css">
+	</head>
+	<body>
+		<header class="navbar">
+			<div class="wrapper">
+				<div class="icon">
+					<pu:trans phrase="title">
+				</div>
+			</div>
+		</header>
+		<article class="wrapper">
+			<pu:module>
+		</article>
+	</body>
+</html>
---
+++ src/themes/barista/theme.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Barista theme for Plutonium
+ * @package Plutonium Barista Theme
+ */
+
+use Plutonium\Application\Theme;
+
+class BaristaTheme extends Theme {}
