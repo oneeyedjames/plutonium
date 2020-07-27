46358c0fb92b49e7e086fd51ee3ca997741e3652
J Andrew Scott <jascott@programmer.net>
2020-07-27 15:01:00 -0400

    Moved static assets to shared folder

--- src/themes/barista/layouts/default.html.php
+++ src/themes/barista/layouts/default.html.php
@@ -2,18 +2,34 @@
 <html>
 	<head>
 		<pu:head>
-		<link rel="stylesheet" href="themes/barista/assets/barista.min.css">
+
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="assets/barista.min.css">
+		<script type="text/javascript" src="assets/barista.min.js"></script>
 	</head>
 	<body>
 		<header class="navbar">
 			<div class="wrapper">
-				<div class="icon">
+				<a class="icon" href="/">
 					<pu:trans phrase="title">
-				</div>
+				</a>
 			</div>
 		</header>
 		<article class="wrapper">
-			<pu:module>
+			<div class="row">
+				<div class="col-md-4 col-lg-3">
+					<pu:widgets location="sidebar">
+				</div>
+				<div class="col-md-8 col-lg-9">
+					<pu:widgets location="header">
+					<pu:module>
+				</div>
+				<div class="col-xs-12">
+					<pu:widgets location="footer">
+				</div>
+			</div>
 		</article>
 	</body>
 </html>
